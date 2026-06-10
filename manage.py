727e4b7a95b0c2b7d971851cc335da0f95d726e7
RenBriones03 <renato.briones@inacapmail.cl>
2026-06-10 16:20:45 -0400

    Revision Base de datos

---
+++ manage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retail_chile.settings')
+    #Cositas de problemas de versiones por alguna razón
+    from django.db.backends.base.base import BaseDatabaseWrapper
+    from django.db.backends.mysql.features import DatabaseFeatures
+    from django.db.backends.base.base import BaseDatabaseWrapper
+    BaseDatabaseWrapper.check_database_version_supported = lambda self: None
+    DatabaseFeatures.can_return_rows_from_bulk_insert = property(lambda self: False)
+    DatabaseFeatures.can_return_columns_from_insert = property(lambda self: False)
+    #fin de cosas de problemas de version
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
---
+++ retail_chile/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from retail_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ingestar/', views.ingestar_venta_manual, name='ingestar_venta_manual'),
+]
